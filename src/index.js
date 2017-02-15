887887e464c573eb5a29ce2f15ed85778d5ad478
Dartonian <dartonian@inbox.ru>
2017-02-15 23:11:14 +0300

    add redux todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 'use strict';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+/*-----------------------------------*/
+
+import App from './containers/App';
+import todoApp from './reducers';
+
+/*-----------------------------------*/
+
 import Html from './index.html'
 import Style from './less/styles.less';
+
+let store = createStore(todoApp);
+
+let rootElement = document.getElementById('root');
+React.render(
+  // Дочерний компонент должен быть обернуть в функцию
+  // это баг в React 0.13.
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
+  rootElement
+);
